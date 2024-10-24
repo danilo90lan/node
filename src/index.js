fbbfc1e11bc3fc0b928054201b153f52e469fb94
danilo90lan <15630@coderacademy.edu.au>
2024-10-24 20:47:23 +1100

    added do while loop

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const inquirer = require("inquirer").default;
+// const inquirer = require("inquirer").default;
+const promptSync = require("prompt-sync")();
 
 console.log("Dice Roller Application starts...")
 
@@ -8,6 +9,9 @@ console.log("Dice Roller Application starts...")
  * @returns {number} The side of the dice rolled
  */
 function rollDice(diceSide=6) {
+    if (diceSide <= 0) {
+        throw new Error("Dice side must be greater than 0.");
+    }
     let diceResult = 0;
     diceResult = Math.ceil(Math.random() * diceSide);
     return diceResult;
@@ -15,14 +19,36 @@ function rollDice(diceSide=6) {
 
 // console.log("The side you rolled is: " + rollDice());
 
-const prompt = inquirer.createPromptModule();
+// const prompt = inquirer.createPromptModule();
 
-prompt([
-    {
-        type: "number",
-        name: "dice_side",
-        message: "How many sided dice do you want to roll?"
-    }
-]).then((answer) => {
-    console.log(rollDice(answer.dice_side))
-});
+// prompt([
+//     {
+//         type: "number",
+//         name: "dice_side",
+//         message: "How many sided dice do you want to roll?"
+//     }
+// ]).then((answer) => {
+//     console.log(rollDice(answer.dice_side))
+// });
+
+function getDiceSide(){
+    do {
+        const diceSide = promptSync("How many sided dice do you want to roll? ");
+        try {
+            const parsedDiceSide = parseInt(diceSide);
+            if (isNaN(parsedDiceSide)) {
+                throw new Error('Please enter a valid number.');
+            }
+            const diceResult = rollDice(parsedDiceSide);
+            console.log(`You rolled a ${diceResult}-sided dice.`);
+        } catch (error) {
+            console.error("You got an error!");
+            console.log(error);
+        } finally {
+            console.log("Dice roll operation completed.")
+        }
+    } while (promptSync('Do you want to roll again? (y/n) ').toLocaleLowerCase() === 'y');
+    
+}
+
+getDiceSide();
